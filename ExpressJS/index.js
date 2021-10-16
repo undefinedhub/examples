29fc1fc5c4f7510774955fd1de7268bc2b253f18
zlures <81084648+zlures@users.noreply.github.com>
2021-10-16 17:49:07 +0300

    Update index.js

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,1 +1,12 @@
+const express = require("express"); // Requsting module
+const app = express(); // Creating an app
 
+app.use(express.static("public")); // Making items (stylesheets, scripts) can be accessed in main.
+
+app.get("/", (request, response) => { // If person is requesting the main page (yourdomain.com/)
+  response.sendFile(__dirname + "/views/index.html"); // Send page
+}); // Close syntax
+
+app.get("*", (request, response) => { // If other send
+  response.sendFile(__dirname + "/views/404.html"); // this 404 page
+}); // Syntax again
